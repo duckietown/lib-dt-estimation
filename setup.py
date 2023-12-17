f05db16dfe92dede4442eafc6ba04b57da5d4432
Andrea F. Daniele <afdaniele@duckietown.com>
2023-12-17 16:29:29 -0500

    updated URLs: duckietown.org -> duckietown.com

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 package_name = "dt-state-estimation"
 library_webpage = f"http://github.com/duckietown/lib-{package_name}"
 maintainer = "Andrea F. Daniele"
-maintainer_email = "afdaniele@duckietown.org"
+maintainer_email = "afdaniele@duckietown.com"
 short_description = "State estimation components of Duckietown's autonomy behavior."
 full_description = """
 State estimation components of the autonomous behavior pipeline running on Duckietown robots.
